c85637d0d6c80f738648e7196e1d587236dcdf0f
snowykte0426 <snowykte0426@naver.com>
2026-04-29 14:37:09 +0900

    update: 파비콘 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: '블루베리 - 광주SW마이스터고 환영 시스템',
+  icons: { icon: '/favicon.svg' },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
